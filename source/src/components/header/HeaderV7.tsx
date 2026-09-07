40e562302f5bf87fdf7f454eb8237d91994fa1e3
MIT-1708-PATEL <mitpatel@nenotechnology.com>
2026-09-07 16:20:09 +0530

    fix nav bar

--- source/src/components/header/HeaderV7.tsx
+++ source/src/components/header/HeaderV7.tsx
@@ -11,23 +11,26 @@ interface DataType {
 const HeaderV7 = ({ isOpen, openMenu, closeMenu, isMenuSticky, isHomePill }: DataType & { isHomePill?: boolean }) => {
     return (
         <>
-            <header>
+            <header className="header-v7-wrapper">
+                <Link className="header-external-logo" href="/">
+                    <img src="/assets/img/logo/neno%20logo.png" alt="Neno Technology" />
+                </Link>
                 <nav className={`navbar mobile-sidenav navbar-common navbar-sticky navbar-default validnavs navbar-fixed on menu-center no-full glass ${isMenuSticky ? "sticked" : "no-background"} ${isOpen ? "navbar-responsive" : ""} ${isHomePill ? "home-pill-nav" : ""}`}>
 
                     <div className="container d-flex justify-content-between align-items-center">
-                        <div className="navbar-header">
+                        <div className="navbar-header hide-logo">
 
                             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu" onClick={openMenu}>
                                 <i className="fa fa-bars" />
                             </button>
 
                             <Link className="navbar-brand" href="/">
-                                <img src="/assets/img/logo/neno%20logo.png" className="logo" alt="Neno Technology" style={{ height: '90px', width: 'auto', objectFit: 'contain' }} />
+                                <img src="/assets/img/logo/neno%20logo.png" className="logo" alt="Neno Technology" />
                             </Link>
                         </div>
 
-                        <div className={`collapse navbar-collapse ${isOpen ? "show collapse-mobile" : "collapse-mobile"}`} id="navbar-menu">
-                            <img src="/assets/img/logo/neno%20logo.png" alt="Logo" style={{ height: '90px', width: 'auto', objectFit: 'contain' }} />
+                        <div className={`collapse navbar-collapse ${isOpen ? "show collapse-mobile" : "collapse-mobile"} hide-logo`} id="navbar-menu">
+                            <img src="/assets/img/logo/neno%20logo.png" alt="Logo" className="logo" />
 
                             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu" onClick={closeMenu}>
                                 <i className="fa fa-times" />
@@ -40,7 +43,7 @@ const HeaderV7 = ({ isOpen, openMenu, closeMenu, isMenuSticky, isHomePill }: Dat
                             <div className="attr-nav">
                                 <ul>
                                     <li className="button">
-                                        <Link className="btn btn-style-one btn-orange" href="/contact-us">
+                                        <Link className="btn btn-style-one btn-theme" href="/contact-us">
                                             Book A Call <i className="fas fa-arrow-right" />
                                         </Link>
                                     </li>
